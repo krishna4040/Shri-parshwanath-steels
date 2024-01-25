08ce58328bbd02b4cfbe06d2ddc9ef43f0e85fed
krishna jain <115697713+krishna4040@users.noreply.github.com>
2024-01-25 21:04:57 +0530

    Hero images and scrollable nav

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,16 +1,37 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+
+    const navRef = useRef<HTMLDivElement>(null);
+
+    const func = () => {
+        let prevScrollpos = window.pageYOffset;
+        window.onscroll = function () {
+            const currentScrollPos = window.pageYOffset;
+            if (navRef.current) {
+                if (prevScrollpos > currentScrollPos) {
+                    navRef.current.classList.remove("nav-hide");
+                } else {
+                    navRef.current.classList.add("nav-hide");
+                }
+            }
+            prevScrollpos = currentScrollPos;
+        };
+    }
+
+    useEffect(() => {
+        func();
+    }, [window.pageYOffset]);
+
     return (
-        <nav className='flex items-center justify-between bg-[#121316] sticky inset-0 py-5 opacity-60 text-white backdrop-blur-xl z-10'>
+        <nav className='flex items-center justify-between bg-[#121316] fixed top-0 left-0 right-0 py-5 text-white z-10 transition-all duration-200' ref={navRef}>
             <ul className='flex items-center justify-start w-[1440px] mx-auto gap-5'>
                 <li>Shri Parshwanath Steels</li>
                 <Link to={'/'}><li>Home</li></Link>
                 <Link to={'/products'}><li>Products</li></Link>
             </ul>
         </nav>
-
     )
 }
 
--- src/components/landing/components/Hero.tsx
+++ src/components/landing/components/Hero.tsx
@@ -1,21 +1,30 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
-import { Autoplay } from 'swiper/modules';
-
-const COUNT = 10;
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Autoplay, Navigation, Pagination } from 'swiper/modules';
 
 const Hero = () => {
 
     const [photos, setPhotos] = useState([]);
 
     const fecthRandomImage = async () => {
-        const url = `https://api.unsplash.com/photos/random/?client_id=rwfITZE3pW4SSzIzUU_-fRTJIIMLOA33AqoJmtEbGP0&count=10`;
-        const { data } = await axios.get(url);
-        console.log(data);
-        const photoCollection = data.map((photo: any) => photo.urls.small);
-        setPhotos(photoCollection);
+        try {
+            const options = {
+                query: 'metal',
+                count: 10,
+                orientation: 'squarish'
+            }
+            const url = `${import.meta.env.VITE_BASEURL}/search/photos/?client_id=${import.meta.env.VITE_ACCESS_KEY}&count=${options.count}&query=${options.query}&orientation=${options.orientation}`;
+            const { data } = await axios.get(url);
+            const photoCollection = data.results.map((photo: any) => photo.urls.full);
+            console.log(photoCollection);
+            setPhotos(photoCollection);
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     useEffect(() => {
@@ -29,27 +38,25 @@ const Hero = () => {
                 <h1 className='text-[80px] leading-[80px] text-white'>The Morden Platform For Growth on your terms</h1>
                 <button className='bg-white text-black py-[7px] px-[18px] text-xs rounded-full'>Checkout Product Page</button>
             </div>
-            <div>
+            <div className='w-full mx-auto'>
                 <Swiper
-                    modules={[Autoplay]}
-                    slidesPerView={5}
+                    modules={[Autoplay, Pagination]}
+                    slidesPerView={3}
+                    spaceBetween={0}
                     autoplay
+                    pagination
+                    loop
                 >
                     {
                         photos.map((photo, idx) => {
                             return (
                                 <SwiperSlide key={idx}>
-                                    <div className='w-[100px] h-[100px]'><img src={photo} alt="photo" className='w-full' /></div>
+                                    <img src={photo} alt="photo" className='rounded-lg max-h-[400px]' />
                                 </SwiperSlide>
                             )
                         })
                     }
                 </Swiper>
-            </div>
-            <div>
-                <p className='text-[#ffffff]'>We are Eliminating the friction and basis of traditional financing</p>
-                <p className='text-[#84889a]'>Connecting buissness builders ro quick, easy capital and</p>
-                <p className='text-[#84889a]'>helping small to mid-size buissness build someting bigger</p>
             </div>
         </div>
     )
